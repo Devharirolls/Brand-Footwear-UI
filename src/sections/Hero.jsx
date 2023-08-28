4f449d3c84e305b6dc8757e6c79922dcbdd02151
Hariharan <138101854+Devharirolls@users.noreply.github.com>
2023-08-28 22:40:26 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
             The New Arrival
           </span>
           <br />
-          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+          <span className='text-coral-red inline-block mt-3'>JHNike</span> Shoes
         </h1>
         <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
           Discover stylish Nike arrivals, quality comfort, and innovation for
