36bfddf3a9d2ece4127c8b82358c57a05b4af02d
Devharirolls <harirolls183@gmail.com>
2023-08-28 22:19:18 +0530

    TailwindCss

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+
+export{
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+}
